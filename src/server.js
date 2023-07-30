684dbe0591aa513af7f30567b155dafa39f9b4eb
ronyumbu <inidiaakunrony2021@gmail.com>
2023-07-30 20:18:12 +0800

    server-side caching implemented

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const Inert = require('@hapi/inert');
-const path = require('path');
 
 // notes
 const notes = require('./api/notes');
@@ -34,15 +33,17 @@ const ExportsValidator = require('./validator/exports');
 
 // uploads
 const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
+// cache
+const CacheService = require('./services/redis/CacheService');
+
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -123,7 +124,7 @@ const init = async () => {
     {
       plugin: uploads,
       options: {
-        service: storageService,
+        service: uploads,
         validator: UploadsValidator,
       },
     },
